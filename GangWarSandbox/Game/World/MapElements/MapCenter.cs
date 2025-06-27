9a8c357d5dde8e3d8ea9903fc0f7c27c8d89b200
iracheck <137561004+iracheck@users.noreply.github.com>
2025-06-27 10:54:24 -0400

    Refactor file structure

---
+++ GangWarSandbox/Game/World/MapElements/MapCenter.cs
@@ -0,0 +1,37 @@
+﻿using GTA.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GangWarSandbox
+{
+    static class MapCenter
+    {
+        static Vector3 Location;
+
+        static float KillRadius;
+        static float Radius;
+
+        static bool IsVisible;
+
+        static void OnBattleStart()
+        {
+            
+            SetFocalPoint();
+
+        }
+
+        static void OnBattleEnd()
+        {
+
+        }
+
+
+        static void SetFocalPoint()
+        {
+
+        }
+    }
+}
